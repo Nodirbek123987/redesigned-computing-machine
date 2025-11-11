25bcc399fb0aad7bfd4f2f2d23d8aa97f13727ef
Nodirbek <nodirbeksharipov005@gmail.com>
2025-11-11 16:02:05 +0500

    Добавил компоненты и стили

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Restaurants from "@/components/Restaurants";
+import Nearby from "@/components/Nearby";
+import Specialities from "@/components/Specialities";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Restaurants />
+      <Nearby />
+      <Specialities />
+    </>
+  );
+}
